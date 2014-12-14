e985662f82380a6a9f5555a4f8a8927ffcfa78f4
Lê Phương <phuongle0412@gmail.com>
2014-12-14 23:50:57 +0100

    Adding the Lightbox Effect

--- wp-content/themes/onetone/header.php
+++ wp-content/themes/onetone/header.php
@@ -11,6 +11,17 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <title><?php wp_title('|', true, 'right'); ?></title>
 <?php wp_head(); ?>
+<link rel="stylesheet" type="text/css" media="screen" href="http://cdnjs.cloudflare.com/ajax/libs/fancybox/1.3.4/jquery.fancybox-1.3.4.css" />
+<style type="text/css">
+    a.fancybox img {
+        border: none;
+        box-shadow: 0 1px 7px rgba(0,0,0,0.6);
+        -o-transform: scale(1,1); -ms-transform: scale(1,1); -moz-transform: scale(1,1); -webkit-transform: scale(1,1); transform: scale(1,1); -o-transition: all 0.2s ease-in-out; -ms-transition: all 0.2s ease-in-out; -moz-transition: all 0.2s ease-in-out; -webkit-transition: all 0.2s ease-in-out; transition: all 0.2s ease-in-out;
+    } 
+    a.fancybox:hover img {
+        position: relative; z-index: 999; -o-transform: scale(1.03,1.03); -ms-transform: scale(1.03,1.03); -moz-transform: scale(1.03,1.03); -webkit-transform: scale(1.03,1.03); transform: scale(1.03,1.03);
+    }
+</style>
 </head>
 <body <?php body_class(); ?>>
 <?php if(is_home()){$wrap_class = "home-site";}else{$wrap_class = "site";}?>
