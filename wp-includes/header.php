13f6d020d15699bd0b9b379ae4071c30e7da5e01
Lê Phương <phuongle0412@gmail.com>
2014-12-16 22:16:03 +0100

    Changed abit in layout, effects
    and update links to social networks on the icons in Home

---
+++ wp-includes/header.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * The header for home page.
+ *
+ */
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<title><?php wp_title('|', true, 'right'); ?></title>
+<?php wp_head(); ?>
+<link rel="stylesheet" type="text/css" media="screen" href="http://cdnjs.cloudflare.com/ajax/libs/fancybox/1.3.4/jquery.fancybox-1.3.4.css" />
+<style type="text/css">
+    a.fancybox img {
+        border: none;
+        box-shadow: 0 1px 7px rgba(0,0,0,0.6);
+        -o-transform: scale(1,1); -ms-transform: scale(1,1); -moz-transform: scale(1,1); -webkit-transform: scale(1,1); transform: scale(1,1); -o-transition: all 0.2s ease-in-out; -ms-transition: all 0.2s ease-in-out; -moz-transition: all 0.2s ease-in-out; -webkit-transition: all 0.2s ease-in-out; transition: all 0.2s ease-in-out;
+    } 
+    a.fancybox:hover img {
+        position: relative; z-index: 999; -o-transform: scale(1.03,1.03); -ms-transform: scale(1.03,1.03); -moz-transform: scale(1.03,1.03); -webkit-transform: scale(1.03,1.03); transform: scale(1.03,1.03);
+    }
+</style>
+<link href="css/hover.css" rel="stylesheet" media="all">
+</head>
+<body <?php body_class(); ?>>
+<?php if(is_home()){$wrap_class = "home-site";}else{$wrap_class = "site";}?>
+<div class="<?php echo $wrap_class;?>">
+	<header class="home-header">
+		<div class="home-logo onetone-logo ">
+        	<a href="<?php echo esc_url(home_url('/')); ?>">
+        <?php if ( onetone_options_array('logo')!="") { ?>
+        <img src="<?php echo onetone_options_array('logo'); ?>" alt="<?php bloginfo('name'); ?>" />
+        <?php }else{ ?>
+        <span class="site-name">
+        <?php bloginfo('name'); ?>
+        </span>
+        <?php }?>
+        </a>
+        <?php if ( 'blank' != get_header_textcolor() && '' != get_header_textcolor() ){?>
+        <div class="site-description "><?php bloginfo('description'); ?></div>
+        <?php }?>
+        </div>
+        
+        <a class="home-navbar navbar" href="javascript:;"></a>
+        <nav class="home-navigation top-nav">
+<?php
+
+ $onepage_menu = '';
+ $section_num = onetone_options_array( 'section_num' ); 
+ if(isset($section_num) && is_numeric($section_num ) && $section_num >0):
+ for( $i = 0; $i < $section_num ;$i++){
+
+ $section_menu = onetone_options_array( 'menu_title_'.$i );
+ $section_slug = onetone_options_array( 'menu_slug_'.$i );
+
+ if(isset($section_menu) && $section_menu !=""){
+ $sanitize_title = sanitize_title($section_menu);
+ $section_menu = onetone_options_array( 'menu_title_'.$i );
+ if(trim($section_slug) !=""){
+	 $sanitize_title = $section_slug; 
+	 }
+ $onepage_menu .= '<li  class="onetone-menuitem"><a id="onetone-'.$sanitize_title.'" href="#'.$sanitize_title.'" >
+ <span>'.$section_menu.'</span></a></li>';
+ }
+ }
+endif;
+if ( has_nav_menu( "home_menu" ) ) {
+ wp_nav_menu(array('theme_location'=>'home_menu','depth'=>0,'fallback_cb' =>false,'container'=>'','container_class'=>'main-menu','menu_id'=>'menu-main','menu_class'=>'main-nav','link_before' => '<span>', 'link_after' => '</span>','items_wrap'=> '<ul id="%1$s" class="%2$s">'.$onepage_menu.'%3$s</ul>'));
+}
+else{
+echo '<ul>'.$onepage_menu.'</ul>';
+}
+?>
+        </nav>
+		<div class="clear"></div>
+	</header>    
+	<!--header-->
