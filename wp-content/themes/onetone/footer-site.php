3cc14fb414256763ec0fc27d89bfbbe4c8fc0b55
Lê Phương <phuongle0412@gmail.com>
2015-01-04 15:45:00 +0100

    Changed link on footer-site
    When click on the link, it will link to About

--- wp-content/themes/onetone/footer-site.php
+++ wp-content/themes/onetone/footer-site.php
@@ -1,6 +1,6 @@
 <footer class="site-footer">
 		<div class="site-info">
-        	<?php printf(__('Powered by <a href="%s">WordPress</a>. Designed by <a href="%s">MageeWP Themes</a>.','onetone'),esc_url('http://wordpress.org/'),esc_url('http://www.mageewp.com/'));?>
+        	<?php printf(__(' <a href="%s"></a>Designed by <a href="%s">Phuong and Helene </a>.','onetone'),esc_url('http://master1realiteaugmentee.16mb.com/#about-us'),esc_url('http://master1realiteaugmentee.16mb.com/#about-us'));?>
 		</div>
 	</footer>
 </div>
